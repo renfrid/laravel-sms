561dd77ec26efaa173c9d12e82d9c482815475b4
renfrid <ngolongolo@multics.co.tz>
2022-12-23 10:37:10 +0300

    Minor change

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -151,7 +151,7 @@ class CronJobController extends Controller
     function delivery_report()
     {
         //limit
-        $limit = 1000;
+        $limit = 2000;
 
         //date range
         $start_at = date('2022-12-19');
@@ -159,8 +159,8 @@ class CronJobController extends Controller
 
         //take all recipients for delivery report
         $recipients = SmsLog::select('id', 'gateway_id', 'phone')->where(function ($query) {
-            $query->where(['gateway_status' => 'SENT'])
-                ->orWhere(['gateway_status' => 'PENDING']);
+            $query->where(['gateway_status' => 'SENT']);
+                //->orWhere(['gateway_status' => 'PENDING']);
         })->whereBetween('sms_logs.created_at', [$start_at, $end_at])->take($limit)->get();
 
         foreach ($recipients as $val) {
